21feffefe76026d590579d0fb6cb68662ca27bcf
simontetteh29-tech <simontetteh29@gmail.com>
2026-05-26 14:58:29 +0000

    Update gcgzmap.py

--- gcgzmap.py
+++ gcgzmap.py
@@ -17,7 +17,10 @@ st.title("GCG Operational Zonal Offices Dashboard")
 # ==================================================
 @st.cache_data
 def load_data():
-    gdf = gpd.read_file("gadm41_GHA_2.json.zip")
+    from pathlib import Path
+
+file_path = Path(__file__).parent / "gadm41_GHA_2.json.zip"
+gdf = gpd.read_file(file_path)
     return gdf.to_crs(epsg=4326)
 
 gdf = load_data()
